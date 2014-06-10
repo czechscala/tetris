bc4b02d39de1986509314f8923dd98fceb4d07fe
Karel Smutný <karel.smutny@hotmail.com>
2014-06-10 20:54:22 +0200

    tests for Engine speed increase

--- src/main/scala/com/czechscala/tetris/Tetris.scala
+++ src/main/scala/com/czechscala/tetris/Tetris.scala
@@ -4,25 +4,40 @@ import model._
 import akka.actor.{Props, ActorSystem, Actor}
 import scala.annotation.tailrec
 import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits._
+import com.czechscala.tetris.render.Swing
+
 
 object Tetris extends App {
 
+  case object Tick
   case class Key(code: Int)
 
   class Engine extends Actor {
+    private val renderer = new Swing
     private var state = State(Board(10, 20, Set()), None)
     private var ticks = 0
-    private var speed = 2000
+    private var delay = 2000
 
-    override def preStart = context.system.scheduler.scheduleOnce(speed millis, self, "tick")
+    override def preStart = {
+      renderer render state
+      tick
+    }
+
+    private def tick = context.system.scheduler.scheduleOnce(delay millis, self, Tick)
 	
     def receive = {
-      case "tick" =>
+      case Tick =>
+        renderer render state
         ticks += 1
-        if (ticks == 100) speed *= 0.9
+        if (ticks % 100 == 0) delay = (delay * 0.9).toInt
+        tick
 
       case Key(code) => println("key pressed: " + code);
     }
+
+    def getTicks = ticks
+    def getDelay = delay
   }
 
   val system = ActorSystem("tetris")
---
+++ src/test/scala/com/czechscala/tetris/TetrisEngineSuite.scala
@@ -0,0 +1,25 @@
+package com.czechscala.tetris
+
+import org.scalatest.{FlatSpecLike, Matchers}
+import akka.testkit.{TestActorRef, TestKit}
+import akka.actor.ActorSystem
+import Tetris._
+
+class TetrisEngineSuite extends TestKit(ActorSystem()) with FlatSpecLike with Matchers {
+
+  "Engine" should "update ticks number" in {
+    val engine = TestActorRef[Engine]
+    engine.underlyingActor.getTicks should be (0)
+    engine.tell(Tick, testActor)
+    engine.underlyingActor.getTicks should be (1)
+  }
+
+  "Engine" should "decrease delay after 100 ticks" in {
+    val engine = TestActorRef[Engine]
+    engine.underlyingActor.getTicks should be (0)
+    engine.underlyingActor.getDelay should be (2000)
+    1 to 100 foreach { _ => engine.tell(Tick, testActor) }
+    engine.underlyingActor.getTicks should be (100)
+    engine.underlyingActor.getDelay should be (1800)
+  }
+}
