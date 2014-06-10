4075dbbd7d665e946dd6dbaa1885272e59ba85c2
Janeček Jakub <janecek@avast.com>
2014-06-10 21:08:01 +0200

    Render current state shape

--- src/main/scala/com/czechscala/tetris/model.scala
+++ src/main/scala/com/czechscala/tetris/model.scala
@@ -6,22 +6,30 @@ package object model {
 
   case class Board(width: Int, height: Int, grid: Grid) {
     require {
-      grid forall { case (x, y) => x >= 0 && x < width && y >= 0 && y < height }
+      grid forall { case (x, y) => x >= 0 && x < width && y >= 0 && y < height}
     }
   }
+
+  sealed trait Direction
+
+  case object Down extends Direction
+
+  case object Left extends Direction
+
+  case object Right extends Direction
 
   case class Shape(grid: Grid) {
 
     require {
-      !grid.filter { case (x, y) => x == 0 || y == 0 }.isEmpty
+      !grid.filter { case (x, y) => x == 0 || y == 0}.isEmpty
     }
 
     def width: Int = {
-      grid.map { case (x, y) => x }.max + 1
+      grid.map { case (x, y) => x}.max + 1
     }
 
     def height: Int = {
-      grid.map { case (x, y) => y }.max + 1
+      grid.map { case (x, y) => y}.max + 1
     }
   }
 
@@ -36,11 +44,11 @@ package object model {
 
   sealed trait KeyPress
 
-  case object Left extends KeyPress
+  case object LeftArrow extends KeyPress
 
-  case object Right extends KeyPress
+  case object RightArrow extends KeyPress
 
-  case object Down extends KeyPress
+  case object DownArrow extends KeyPress
 
   case object Space extends KeyPress
 
